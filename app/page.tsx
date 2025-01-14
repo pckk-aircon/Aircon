d4b11dcd7a4c56c0ce0e038873fc7113b410a3e1
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-14 15:04:05 +0900

    step5-addget

--- app/page.tsx
+++ app/page.tsx
@@ -64,10 +64,11 @@ export default function App() {
   }
 
   async function getPost() {
-    //const postId = window.prompt("Enter post ID");
-    const {data} = await client.queries.getPost({
+    const response = await client.queries.getPost({
       id: "a12b2004-a0ac-4dbe-9d90-00942a285a09",
       });
+    console.log(response)
+    const { data } = response;
     console.log(data)
   }
 
