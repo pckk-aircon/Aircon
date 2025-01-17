768797ceffc86c0cbd9acaa9a3a0b27374ec9d43
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-17 16:27:07 +0900

    step5-add

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
+
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
@@ -41,7 +42,6 @@ export default function App() {
   }, []);
 
   
-
   function createTodo() {
     client.models.Todo.create({
       content: window.prompt("Todo content"),
