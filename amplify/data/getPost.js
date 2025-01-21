1b6e7fbea7dfbbcb017a25505aba6a3c9a0083e3
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-21 14:05:43 +0900

    key

--- amplify/data/getPost.js
+++ amplify/data/getPost.js
@@ -1,21 +1,8 @@
 //step4にて追加。
-//import * as ddb from "@aws-appsync/utils/dynamodb";
+import * as ddb from "@aws-appsync/utils/dynamodb";
 
-//export function request(ctx) {
+export function request(ctx) {
 
-  //return ddb.get({ key: { id: ctx.args.id } });
+  return ddb.get({ key: { id: ctx.args.id } });
 
-//}
-
-import { util } from '@aws-appsync/utils';
-
-export function request() {
-  //const { owner } = ctx.args;
-  return {
-    operation: 'Query',
-    query: {
- 
-    },
-    index: 'Controller-index'
-  };
 }
