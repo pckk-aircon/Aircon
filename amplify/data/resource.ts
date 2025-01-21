f34f89daa3ae48b8dc3097b794da854eaa95c626
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-21 17:45:46 +0900

    DeviceTable-Device

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -49,8 +49,8 @@ const schema = a.schema({
   getPost: a
     .query()
     .arguments({
-      Device: a.id().required(),//re
-      Controller: a.string() // Controllerを追加
+      Device: a.id().required(),
+      //Controller: a.string() // Controllerを追加
     })
     .returns(a.ref("Post"))
     .authorization(allow => [allow.publicApiKey()])
