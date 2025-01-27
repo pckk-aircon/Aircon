93a3793aea074f3503bc30ac6d9ccaf5a1311207
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-27 17:40:31 +0900

    Iot-page

--- amplify/data/listIotDataByController.js
+++ amplify/data/listIotDataByController.js
@@ -3,7 +3,9 @@
 import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
+    console.log('handrar called'); // 関数が呼び出されたことを確認
     return {
+
         operation: 'Query',
         query: {
             //expression: 'Controller = :controller AND DeviceDatetime = :deviceDatetime',
