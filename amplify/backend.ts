20df60a54c1c4f42d8f3f85eb802051525e3fc18
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-27 18:29:32 +0900

    Iot-page

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -17,8 +17,9 @@ const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
 const externalTable = aws_dynamodb.Table.fromTableName(
   externalDataSourcesStack,
   "MyExternalPostTable",
+  "DeviceTable"
   //"IotData"
-  "DivisionTable"
+  //"DivisionTable"
 
 );
 
