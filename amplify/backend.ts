1ad7f2c765ac6e29f9f223196a7d9fede4929073
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-01-27 11:09:26 +0900

    Iot-cont

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -20,6 +20,13 @@ const externalTable = aws_dynamodb.Table.fromTableName(
   "DeviceTable"
 );
 
+//新しいテーブル（IoTData）の設定を追加
+const iotDataTable = aws_dynamodb.Table.fromTableName(
+  externalDataSourcesStack,
+  "MyIotDataTable",
+  "IotData"
+);
+
 //backend.data.addDynamoDbDataSource(
   //"ExternalPostTableDataSource",
   //externalTable
@@ -40,11 +47,24 @@ const externalTableDS = backend.data.addDynamoDbDataSource(
   externalTable
 );
 
+//新しいテーブル（IoTData）の設定を追加
+const iotDataTableDS = backend.data.addDynamoDbDataSource(
+  "IotDataTableDataSource",
+  iotDataTable
+);
+
 const dsRole = Role.fromRoleArn(
   externalDataSourcesStack,
   "DatasourceRole",
   externalTableDS.ds.serviceRoleArn ?? ''
 )
+
+//新しいテーブル（IoTData）の設定を追加
+const iotDataRole = Role.fromRoleArn(
+  externalDataSourcesStack,
+  "IotDataRole",
+  iotDataTableDS.ds.serviceRoleArn ?? ''
+);
 
 const datasourceIamPolicy = new Policy(externalDataSourcesStack, "datasourceIamPolicy", {
   policyName: "amplify-permissions-external-table",
