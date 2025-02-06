682c507b4aa0c0409d191df6cf2965b878cf47b5
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-06 11:41:11 +0900

    field

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -18,10 +18,11 @@ const schema = a.schema({
     Device: a.id().required(),
     DeviceDatetime: a.string(),
     Controller: a.string(),
-    DeviceType: a.string(),
-    Division: a.string(), 
     ActualTemp: a.string(),
     ActualHumidity: a.string(),
+    DeviceType: a.string(),
+    Division: a.string(), 
+
   }),
 
 
