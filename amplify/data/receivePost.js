a99d9c971425ec7c8497e6bc5fbe9cfad6c3fb1e
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-06 09:05:52 +0900

    field

--- amplify/data/receivePost.js
+++ amplify/data/receivePost.js
@@ -1,7 +1,26 @@
+/*
 export function request() {
   return {};
 }
 
 export const response = (ctx) => {
   return ctx.result;
+};
+*/
+
+
+export function request() {
+  return {};
+}
+
+export const response = (ctx) => {
+  // リクエストからフィールドの値を取得します。
+  const deviceType = ctx.args.DeviceType;
+  const division = ctx.args.Division;
+
+  return {
+    ...ctx.result,
+    DeviceType: deviceType,
+    Division: division
+  };
 };
