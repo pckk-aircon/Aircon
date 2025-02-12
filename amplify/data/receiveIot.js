ddb76162b1b53db54e97ff0a45e80c2b0c931bcc
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-12 14:03:33 +0900

    field

---
+++ amplify/data/receiveIot.js
@@ -0,0 +1,20 @@
+export function request() {
+  // リクエストを返す際には、通常の設定や初期化を行います。
+  return {};
+}
+
+export const response = (ctx) => {
+  // リクエストからフィールドの値を取得。
+  const deviceType = ctx.args.DeviceType;
+  const division = ctx.args.Division;
+  const actualTemp = ctx.args.ActualTemp;
+  const actualHumidity = ctx.args.ActualHumidity;
+
+  return {
+    ...ctx.result,
+    DeviceType: deviceType,
+    Division: division,
+    ActualTemp: actualTemp,
+    ActualHumidity: actualHumidity,
+  };
+};
