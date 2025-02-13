c4868e67a6aa58481bee12884971ad453c717eb7
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-13 11:42:46 +0900

    field

--- amplify/data/getList.js
+++ amplify/data/getList.js
@@ -1,20 +1,3 @@
-/*
-import { util } from "@aws-appsync/utils";
-import * as ddb from "@aws-appsync/utils/dynamodb";
-
-export function request(ctx) {
-  const item = { ...ctx.arguments, ups: 1, downs: 0, version: 1 };
-  const key = { Device: ctx.args.Device ?? util.autoId() };
-  return ddb.put({ key, item });
-}
-
-export function response(ctx) {
-  return ctx.result;
-}
-
-*/
-
-
 export function request() {
   return {};
 }
