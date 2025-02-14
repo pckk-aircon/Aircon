348cdbedda5bcd69998ae09be54204ca2d7e4034
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-14 14:48:30 +0900

    grapf

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -195,7 +195,11 @@ export default function App() {
         <ResponsiveContainer width="100%" height={400}>
           <LineChart data={mergedData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" vertical={false} />
-            <XAxis dataKey="DeviceDatetime" />
+            <XAxis
+              dataKey="DeviceDatetime"
+              tickFormatter={(tick) => format(parseISO(tick), "MM-dd HH")}
+              interval={0}
+            />
             <YAxis />
             <Tooltip />
             <Legend />
