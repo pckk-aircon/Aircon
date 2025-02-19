ca7b6811a05a3f90a3e3cba7d96db1ee36a9a944
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-19 14:06:02 +0900

    graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,25 @@ import Sidebar from './Sidebar'; // Sidebar.tsxへのパスを調整してくだ
 
 import { Authenticator } from "@aws-amplify/ui-react"; //login認証
 import "@aws-amplify/ui-react/styles.css"; //login認証
-import { useForm, FormProvider, useFormContext } from 'react-hook-form'; //login認証
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+
   return (
     <html lang="ja">
       <body>
         <div style={{ display: 'flex' }}>
           <Sidebar />
           <div style={{ flex: 1 }}>
-            <Authenticator>
-              {children}
-            </Authenticator>
+            {children}
           </div>
         </div>
       </body>
     </html>
   );
+
+
 }
