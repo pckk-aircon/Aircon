7e91ef28fc35005855f392ced6eec3780f99988d
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-02-21 09:19:56 +0900

    graph

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -481,7 +481,7 @@ export default function App() {
               tickFormatter={formatXAxis} 
               angle={45} 
               textAnchor="end" 
-              height={35} 
+              height={0} 
               //interval={0} // すべてのラベルを表示。1にするとうまくいかない。
             />
 
