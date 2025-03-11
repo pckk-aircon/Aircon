936ee77397f252f3320487f4f3c2a88916efe6e1
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-03-11 16:44:26 +0900

    red

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -309,7 +309,7 @@ const TerrainMap: FC = () => {
         const camera = new THREE.PerspectiveCamera(75, map.getCanvas().width / map.getCanvas().height, 0.1, 1000);
 
         camera.position.set(x + cameraDistance, y + cameraDistance, z + cameraDistance);// 球体の位置から少し離れた位置にカメラを配置
-        camera.lookAt(-4002585.05, 3322656.83, 3690534.34); // 球体の位置を向く
+        camera.lookAt(x, y, z); // 球体の位置を向く
 
         // 赤い球体の作成
         const geometry = new THREE.SphereGeometry(5, 32, 32); // 半径5mで直径10mの球体
