931df32fecceac24313fa2699d5664b5f8ef94b5
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-03-24 11:17:02 +0900

    datasource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -20,14 +20,6 @@ const externalTable = aws_dynamodb.Table.fromTableName(
   "IotData"
 );
 
-//DeviceTableの設定を追加
-//const iotTable = aws_dynamodb.Table.fromTableName(
-const DeviceTable = aws_dynamodb.Table.fromTableName(//★
-  externalDataSourcesStack,
-  "MyDeviceTable",
-  //"IotData"
-  "DeviceTable"//★テーブル名
-);
 
 //2025.1.23サポート様より提示。
 //addDynamoDbDataSource() により作成されるデータソースには新規のIAMロールが作成される一方、
@@ -38,14 +30,7 @@ import { Role, Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 
 const externalTableDS = backend.data.addDynamoDbDataSource(
   "ExternalPostTableDataSource",
-  //"IotPostTableDataSource",//★★
   externalTable//こちらは変数名。次のRoleと関連か。
-);
-
-//DeviceTableの設定を追加
-const DeviceDS = backend.data.addDynamoDbDataSource(
-  "DeviceDataSource",//データソース名
-  DeviceTable//テーブル名
 );
 
 //dsRoleは、externalTableDSのIAMロールを取得おり、同じロールをDeviceTableにも適用可能。
@@ -66,7 +51,6 @@ const datasourceIamPolicy = new Policy(externalDataSourcesStack, "datasourceIamP
       ],
       resources: [
         `${externalTable.tableArn}/index/*`,
-        `${DeviceTable.tableArn}/index/*`, // DeviceTableのリソースを追加
       ],
     })
   ],
@@ -78,59 +62,54 @@ dsRole.attachInlinePolicy(datasourceIamPolicy);
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-
-import { aws_dynamodb } from "aws-cdk-lib"; //step2にて追加。
+import { aws_dynamodb, Stack } from "aws-cdk-lib";
+import { Role, Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 
 export const backend = defineBackend({
   auth,
   data,
 });
 
-
-//step2にて追加。
 const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
 
-const externalTable = aws_dynamodb.Table.fromTableName(
+function createDynamoDbDataSource(
+  stack: Stack,
+  tableName: string,
+  dataSourceName: string,
+  policyName: string
+) {
+  const table = aws_dynamodb.Table.fromTableName(stack, tableName, "IotData");
+
+  const dataSource = backend.data.addDynamoDbDataSource(dataSourceName, table);
+
+  const role = Role.fromRoleArn(stack, `${dataSourceName}Role`, dataSource.ds.serviceRoleArn ?? '');
+
+  const policy = new Policy(stack, policyName, {
+    policyName: `amplify-permissions-${dataSourceName}`,
+    statements: [
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["dynamodb:Query"],
+        resources: [`${table.tableArn}/index/*`],
+      }),
+    ],
+  });
+
+  role.attachInlinePolicy(policy);
+
+  return dataSource;
+}
+
+const externalTableDS = createDynamoDbDataSource(
   externalDataSourcesStack,
   "MyExternalPostTable",
-  "IotData"
-);
-
-
-//2025.1.23サポート様より提示。
-//addDynamoDbDataSource() により作成されるデータソースには新規のIAMロールが作成される一方、
-// 作成されたIAMロールには許可されるresourcesにindexが含まれていないため、
-//データソースのIAMロールにindexへのQueryアクションの権限を追加。
-
-import { Role, Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
-
-const externalTableDS = backend.data.addDynamoDbDataSource(
   "ExternalPostTableDataSource",
-  externalTable//こちらは変数名。次のRoleと関連か。
+  "datasourceIamPolicy"
 );
 
-//dsRoleは、externalTableDSのIAMロールを取得おり、同じロールをDeviceTableにも適用可能。
-const dsRole = Role.fromRoleArn(
+const iotTableDS = createDynamoDbDataSource(
   externalDataSourcesStack,
-  "DatasourceRole",
-  externalTableDS.ds.serviceRoleArn ?? ''
-)
-
-
-const datasourceIamPolicy = new Policy(externalDataSourcesStack, "datasourceIamPolicy", {
-  policyName: "amplify-permissions-external-table",
-  statements: [
-    new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        "dynamodb:Query"
-      ],
-      resources: [
-        `${externalTable.tableArn}/index/*`,
-      ],
-    })
-  ],
-});
-
-dsRole.attachInlinePolicy(datasourceIamPolicy);
-
+  "MyIotTable",
+  "IotDataSource",
+  "iotDatasourceIamPolicy"
+);
