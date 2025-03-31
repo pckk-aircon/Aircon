4e7e14189f2a5e0355315a082c37e61970b2451f
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-03-31 11:38:18 +0900

    iot-division

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -363,6 +363,7 @@ export default function App() {
   ];
   */
 
+  
   const DeviceLists = ["1234-kaki2", "1234-kaki3"];
   const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
   const [divisionLists, setDivisionLists] = useState<Array<[string, string]>>([]);
