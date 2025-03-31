81d16a000daca23e239e04dfe5e0c00141666757
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-03-31 13:41:38 +0900

    iot-division

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -355,14 +355,17 @@ export default function App() {
   const [currentDivisionIndex, setCurrentDivisionIndex] = useState(0);
   const [currentDeviceIndex, setCurrentDeviceIndex] = useState(0);
 
+  /*
   const divisionLists = [
     {'Division':"MUTS-Flower", 'DivisionName':"花卉室"},
     {'Division':"MUTS-Dining", 'DivisionName':"飲食室"},
     {'Division':"MUTS-Rest", 'DivisionName':"休憩室"},
   ];
+  */
 
   const DeviceLists = ["1234-kaki2", "1234-kaki3"];
-  const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
+  //const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
+  const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -407,7 +410,8 @@ export default function App() {
         )
 
         .map(item => {
-          const divisionName = posts.find(post => post.Division === item?.Division)?.Division || '';
+          //const divisionName = posts.find(post => post.Division === item?.Division)?.Division || '';
+          const divisionName = divisionLists.find(post => post.Division === item?.Division)?.Division || '';
           return {
             DeviceDatetime: item?.DeviceDatetime ?? '',
             ActualTemp: item?.ActualTemp !== undefined && item.ActualTemp !== null ? parseFloat(item.ActualTemp) : null,
