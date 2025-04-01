44ea4763adaa4cea71a8f8d8bdf2d11cba93465b
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-04-01 14:06:37 +0900

    iot-division

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -380,13 +380,14 @@ export default function App() {
     fetchData();
   }, []);//初回レンダリング時にlistPostを実行
 
+  /*
   useEffect(() => {
     async function fetchData() {
       listIot();
     }
     fetchData();
   }, [startDate, endDate, currentDivisionIndex, currentDeviceIndex]);// 依存関係が変更されるたびにlistIotを実行
-
+  */
 
   async function listPost() {
     try {
