0edc020b06e4ec27314db7df40b8a0c0da3b8293
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-04-01 09:44:54 +0900

    iot-division

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -360,19 +360,19 @@ export default function App() {
   const [currentDivisionIndex, setCurrentDivisionIndex] = useState(0);
   const [currentDeviceIndex, setCurrentDeviceIndex] = useState(0);
 
-  
+  /*
   const divisionLists = [
     {'Division':"MUTS-Flower", 'DivisionName':"花卉室"},
     {'Division':"MUTS-Dining", 'DivisionName':"飲食室"},
     {'Division':"MUTS-Rest", 'DivisionName':"休憩室"},
   ];
+  */
 
 
 
   const DeviceLists = ["1234-kaki2", "1234-kaki3"];
-  const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
-  //const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
-  console.log('posts=', posts);
+  //const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
+
 
   useEffect(() => {
     async function fetchData() {
@@ -381,6 +381,9 @@ export default function App() {
     fetchData();
   }, []);
 
+  const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
+  console.log('divisionLists=', divisionLists);
+  
   useEffect(() => {
     async function fetchData() {
       await listIot();
@@ -559,7 +562,7 @@ export default function App() {
       </div>
 
       <div>
-        <h1>Temperature Data for {posts[currentDivisionIndex].DivisionName} _ {DeviceLists[currentDeviceIndex]}</h1>
+        <h1>Temperature Data for {divisionLists[currentDivisionIndex].DivisionName} _ {DeviceLists[currentDeviceIndex]}</h1>
         <ResponsiveContainer width="100%" height={400}>
           <LineChart data={mergedData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="1 1" vertical={false} />
