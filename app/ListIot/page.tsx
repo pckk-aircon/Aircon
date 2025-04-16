67fb92e90b58c5b8a9d0fdc486367e02b80b497e
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-04-16 17:06:46 +0900

    FiltereddeviceLists

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -624,7 +624,7 @@ export default function App() {
         <button onClick={DevicehandleNext}>nextDevice</button>
       </div>
       <div>
-        <h1>Temperature Data for {divisionLists[currentDivisionIndex].DivisionName} _ {deviceLists[currentDeviceIndex].DeviceName}</h1>
+        <h1>Temperature Data for {divisionLists[currentDivisionIndex].DivisionName} _ {FiltereddeviceLists[currentDeviceIndex].DeviceName}</h1>
         <ResponsiveContainer width="100%" height={400}>
           <LineChart data={mergedData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="1 1" vertical={false} />
