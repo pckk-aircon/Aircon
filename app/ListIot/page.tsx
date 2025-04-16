a9a795db9e82921d2d2e04b6288efbf31ab2185b
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-04-16 13:35:13 +0900

    filter-latest

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -381,7 +381,7 @@ export default function App() {
   const [currentDivisionIndex, setCurrentDivisionIndex] = useState(0);
   const [currentDeviceIndex, setCurrentDeviceIndex] = useState(0);
 
-  //const DeviceLists = ["1234-kaki2", "1234-kaki3"];
+  const DeviceLists = ["1234-kaki2", "1234-kaki3"];
 
   const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Controller?: string | null }>>([]);
   const [deviceLists, setDevices] = useState<Array<{ Device: string; DeviceName: string; DeviceType: string; Division: string; Controller?: string | null }>>([]);
@@ -432,15 +432,25 @@ export default function App() {
 
       const formattedData = data
 
-      //deviceのボタンが反応しないが、それ以外はOK
+      /*
+      //DeviceListsにすればOK
       .filter(item => 
         divisionLists?.[currentDivisionIndex]?.Division && // オプショナルチェーンを使用
         item?.Division === divisionLists[currentDivisionIndex].Division && 
         (
           item?.DeviceType === 'Temp' || 
-          (item?.DeviceType === 'Aircon' && deviceLists?.some(device => device?.Device === deviceLists[1]))
+          (item?.DeviceType === 'Aircon' && deviceLists?.some(device => device?.Device === DeviceLists[currentDeviceIndex]))
         )
       )
+      */
+
+      .filter(item => 
+        divisionLists?.[currentDivisionIndex]?.Division && // オプショナルチェーンを使用
+        item?.Division === divisionLists[currentDivisionIndex].Division && 
+        (
+         item?.DeviceType === 'Temp' || 
+        (item?.DeviceType === 'Aircon' && deviceLists?.[currentDeviceIndex]?.Device === item?.Device))
+      ) //ここはグラフ表示部分なので、'Temp'と'Aircon'両方を抽出する。
       
 
         .map(item => {
