fd4688241f712004312a3381070344d4fce7dac3
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-01 17:32:27 +0900

    babylon

--- app/Map2/page.tsx
+++ app/Map2/page.tsx
@@ -202,8 +202,8 @@ const MapWith3DModel: React.FC = () => {
         new BABYLON.AxesViewer(scene, 10);
 
         BABYLON.SceneLoader.LoadAssetContainerAsync(
-          //'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
-          'file:///C:/Users/kiyoshi.tabuchi/Desktop/GIS/34M_17.gltf',
+          'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
+          //'file:///C:/Users/kiyoshi.tabuchi/Desktop/GIS/34M_17.gltf',
           '',
           scene
         ).then((modelContainer) => {
