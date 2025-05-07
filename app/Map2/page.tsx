8f240c326e4062abbe8ec433fb301ae9137f3962
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-07 11:43:54 +0900

    map2

--- app/Map2/page.tsx
+++ app/Map2/page.tsx
@@ -204,14 +204,14 @@ const MapWith3DModel: React.FC = () => {
 
         // URLから.gltfファイルを読み込む
         BABYLON.SceneLoader.LoadAssetContainerAsync(
-          //'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
-
+          'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
+          '',
           //'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/',
           //'34M_17.gltf',
 
-          'https://pckk-device.s3.ap-northeast-1.amazonaws.com/',
-          '34M_17.gltf',
-          
+          //'https://pckk-device.s3.ap-northeast-1.amazonaws.com/',
+          //'34M_17.gltf',
+
           scene
         ).then((modelContainer) => {
           modelContainer.addAllToScene();
