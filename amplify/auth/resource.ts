3465afc0e84b63e9d8b247a2eeac838127270df3
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-12 16:12:44 +0900

    auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -21,11 +21,13 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-    
+  /*  
   userAttributes: {
     preferredUsername: {
       mutable: true,
       required: false
     }
   }
+  */
+
 })
