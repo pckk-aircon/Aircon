461123cf24a4271e0163d6ca4277650612a56739
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-13 13:59:15 +0900

    auth-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function Layout({ children }: LayoutProps) {
 */
 
 import { ReactNode } from 'react';
-import Sidebar from './Sidebar'; // Sidebar.tsxへのパスを調整してください。
+import Sidebar from './Sidebar';
 
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
@@ -47,20 +47,15 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-
 export default function Layout({ children }: LayoutProps) {
   return (
-    <html lang="en">
-      <body>
-
-          <div style={{ display: 'flex' }}>
-            <Sidebar />
-            <div style={{ flex: 1 }}>
-              {children}
-            </div>
-          </div>
-
-      </body>
-    </html>
+    <Authenticator>
+      <main style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ flex: 1 }}>
+          {children}
+        </div>
+      </main>
+    </Authenticator>
   );
 }
