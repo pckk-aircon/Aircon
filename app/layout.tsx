b04c7a872179a95d7f7f83f3b3f50ccb9c9542e6
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-14 17:20:22 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/*
 import { ReactNode } from 'react';
 import Sidebar from './Sidebar'; // Sidebar.tsxへのパスを調整してください。
 
@@ -13,6 +14,43 @@ export default function Layout({ children }: LayoutProps) {
           <Sidebar />
           <div style={{ flex: 1 }}>
             {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
+*/
+
+'use client';
+
+import { ReactNode } from 'react';
+import Sidebar from './Sidebar'; // Sidebar.tsxへのパスを調整してください。
+
+//import { Inter } from "next/font/google";
+//import "./app.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "@/amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+
+// ここで Amplify を初期化
+Amplify.configure(outputs);
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <html lang="ja">
+      <body>
+        <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <div style={{ flex: 1 }}>
+            <Authenticator>
+              {children}
+            </Authenticator>
           </div>
         </div>
       </body>
