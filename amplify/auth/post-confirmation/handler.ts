de415126dd4a706d9b0d197c40c9524070c2ac05
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-22 10:07:49 +0900

    tsconf

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -3,13 +3,13 @@ import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-//import { env } from "$amplify/env/post-confirmation";
+import { env } from "$amplify/env/post-confirmation";
 
-//const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
-  //env
-//);
+const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
+  env
+);
 
-//Amplify.configure(resourceConfig, libraryOptions);
+Amplify.configure(resourceConfig, libraryOptions);
 
 const client = generateClient<Schema>();
 
