1bdb1db25a694c142201bc66fcedf94d27a6851d
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-05-28 13:32:03 +0900

    login

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -4,112 +4,6 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
 
-  //Deviceのデータを設定。
-  Post: a.customType({
-    Device: a.id().required(),
-    Controller: a.string(),
-  }),
-
-  //step3にて追加。
-  addPost: a
-    .mutation()
-    .arguments({
-      Device: a.id(),//page.tsxでのエラーを防ぐため.required()をはずす。
-      Controller: a.string()
-    })
-    .returns(a.ref("Post"))
-    .authorization(allow => [allow.publicApiKey()])
-    .handler(
-      a.handler.custom({
-        dataSource: "DeviceDataSource",
-        entry: "./addPost.js",
-      })
-    ),
-
-    listDevice: a
-    .query()
-    .arguments({
-      Controller: a.string(),
-    })
-    .returns(a.ref("Post").array())
-    .authorization(allow => [allow.publicApiKey()])
-    .handler(
-      a.handler.custom({
-        //dataSource: "ExternalPostTableDataSource",
-        dataSource: "DeviceDataSource",//★★★変更。
-        entry: "./listDeviceByController.js",
-      })
-    ),
-
-  //カスタムサブスクリプションを実装
-  receivePost: a
-    .subscription()
-    .for(a.ref("addPost")) 
-    .authorization(allow => [allow.publicApiKey()])
-    .handler(
-        a.handler.custom({
-            entry: './receivePost.js'
-        })
-    ),
-
-
-  //＊＊＊＊ listIot ＊＊＊＊
-
-  //IoTのデータを設定
-  IotData: a.customType({
-    Device: a.id().required(),
-    DeviceDatetime: a.string(),
-    Controller: a.string(),
-    ControlStage: a.string(),
-    Power: a.string(),
-    WeightedTemp: a.string(),
-    ReferenceTemp: a.string(), 
-    TargetTemp: a.string(),
-    PresetTemp: a.string(),
-    ActualTemp: a.string(),
-    ActualHumidity: a.string(),
-    DeviceType: a.string(),
-    Division: a.string(), 
-  }),
-
-  // listIot（キー部分とキー以外のフィールドを一度に読み込み）
-  listIot: a
-    .query()
-    .arguments({
-      Controller: a.string(),
-      StartDatetime: a.string(),
-      EndDatetime: a.string(),
-    })
-    .returns(a.ref("IotData").array())
-    .authorization(allow => [allow.publicApiKey()])
-    .handler(
-      a.handler.custom({
-        //dataSource: "ExternalPostTableDataSource",
-        dataSource: "IotSource",//★★★変更。
-        entry: "./listIot.js",
-      })
-    ),
-
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
-});
-
-*/
-
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-
-const schema = a.schema({
-
  // ＊＊＊＊ Device ＊＊＊＊
 
   // データを設定。
@@ -271,6 +165,186 @@ const schema = a.schema({
     ),
 
 });
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
+
+*/
+
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
+
+
+const schema = a.schema({
+
+  // ＊＊＊＊ Device ＊＊＊＊
+
+  Device: a.customType({
+    Device: a.id().required(),
+    DeviceName: a.string(),
+    Controller: a.string(),
+    DeviceType: a.string(),
+    Division: a.string(),
+    gltf: a.string(),
+  }),
+
+  addDevice: a
+    .mutation()
+    .arguments({
+      Device: a.id(),
+      DeviceName: a.string(),
+      Controller: a.string(),
+      DeviceType: a.string(),
+      Division: a.string(),
+      gltf: a.string(),
+    })
+    .returns(a.ref("Device"))
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "DeviceDataSource",
+        entry: "./addDevice.js",
+      })
+    ),
+
+  listDevice: a
+    .query()
+    .arguments({
+      Device: a.id(),
+      DeviceName: a.string(),
+      Controller: a.string(),
+      DeviceType: a.string(),
+      Division: a.string(),
+      gltf: a.string(),
+    })
+    .returns(a.ref("Device").array())
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "DeviceDataSource",
+        entry: "./listPostByController.js",
+      })
+    ),
+
+  receiveDevice: a
+    .subscription()
+    .for(a.ref("addDevice"))
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        entry: './receivePost.js'
+      })
+    ),
+
+  // ＊＊＊＊ Division ＊＊＊＊
+
+  Division: a.customType({
+    Division: a.id().required(),
+    DivisionName: a.string(),
+    Geojson: a.string(),
+    Controller: a.string(),
+  }),
+
+  addDivision: a
+    .mutation()
+    .arguments({
+      Division: a.id(),
+      DivisionName: a.string(),
+      Geojson: a.string(),
+      Controller: a.string()
+    })
+    .returns(a.ref("Division"))
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "DivisionDataSource",
+        entry: "./addDivision.js",
+      })
+    ),
+
+  listDivision: a
+    .query()
+    .arguments({
+      Division: a.id(),
+      DivisionName: a.string(),
+      Geojson: a.string(),
+      Controller: a.string(),
+    })
+    .returns(a.ref("Division").array())
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "DivisionDataSource",
+        entry: "./listPostByController.js",
+      })
+    ),
+
+  receiveDivision: a
+    .subscription()
+    .for(a.ref("addDivision"))
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        entry: './receivePost.js'
+      })
+    ),
+
+  // ＊＊＊＊ Iot ＊＊＊＊
+
+  IotData: a.customType({
+    Device: a.id().required(),
+    DeviceDatetime: a.string(),
+    Controller: a.string(),
+    ControlStage: a.string(),
+    CumulativeEnergy: a.string(),
+    Power: a.string(),
+    WeightedTemp: a.string(),
+    ReferenceTemp: a.string(),
+    TargetTemp: a.string(),
+    PresetTemp: a.string(),
+    ActualTemp: a.string(),
+    ActualHumidity: a.string(),
+    DeviceType: a.string(),
+    Division: a.string(),
+  }),
+
+  listIot: a
+    .query()
+    .arguments({
+      Controller: a.string(),
+      StartDatetime: a.string(),
+      EndDatetime: a.string(),
+    })
+    .returns(a.ref("IotData").array())
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "IotDataSource",
+        entry: "./listIot.js",
+      })
+    ),
+
+  // ＊＊＊＊ UserProfile ＊＊＊＊
+
+  UserProfile: a
+    .model({
+      email: a.string(),
+      profileOwner: a.string(),
+    })
+    .authorization((allow) => [
+      allow.ownerDefinedIn("profileOwner"),
+    ]),
+
+}).authorization((allow) => [allow.resource(postConfirmation)]);
 
 export type Schema = ClientSchema<typeof schema>;
 
