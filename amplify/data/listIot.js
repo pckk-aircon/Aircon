0f7e49804fae93683e77db63e1949127e500d5e5
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-06-04 19:07:14 +0900

    enddate-between

--- amplify/data/listIot.js
+++ amplify/data/listIot.js
@@ -21,7 +21,8 @@ export const response = (ctx) => ctx.result.items;
 
 */
 
-
+/*
+//NG
 export function request(ctx) {
   return {
     operation: 'Query',
@@ -36,3 +37,23 @@ export function request(ctx) {
     index: 'Controller-DeviceDatetime-index'
   };
 }
+*/
+
+import { util } from '@aws-appsync/utils';
+
+export function request(ctx) {
+  return {
+    operation: 'Query',
+    query: {
+      expression: 'Controller = :controller AND DeviceDatetime BETWEEN :startDatetime AND :endDatetime',
+      expressionValues: util.dynamodb.toMapValues({ 
+        ':controller': ctx.args.Controller,
+        ':startDatetime': ctx.args.StartDatetime,
+        ':endDatetime': ctx.args.EndDatetime
+      })
+    },
+    index: 'Controller-DeviceDatetime-index'
+  };
+}
+
+export const response = (ctx) => ctx.result.items;
