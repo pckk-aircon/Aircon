efcb368a053c2778833d2a1fec86ad4f7747e452
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-07-09 15:47:14 +0900

    Division

--- app/TableDivision/page.tsx
+++ app/TableDivision/page.tsx
@@ -1,3 +1,5 @@
+/*
+
 "use client";
 
 import { useState, useEffect } from "react";
@@ -71,3 +73,91 @@ export default function App() {
     </main>
   );
 }
+
+*/
+
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format, parseISO } from "date-fns";
+
+import { useController } from "@/app/context/ControllerContext"; // ← 追加
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const { controller } = useController(); // ← Sidebarで選択されたcontrollerを取得
+  const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; Geojson: string ;Controller?: string | null }>>([]);
+
+  useEffect(() => {
+    listPost();
+  
+    const sub = client.subscriptions.receiveDivision().subscribe({
+      next: (event) => {
+        console.log("event=", event);
+        setPosts((prevPosts) => {
+          if (!prevPosts.some((post) => post.Division === event.Division)) {
+            return [
+              ...prevPosts,
+              event as { 
+                Division: string;
+                DivisionName: string;
+                Geojson: string;
+                Controller?: string | null
+              }
+            ];
+          }
+          return prevPosts;
+        });
+      },
+    });
+    return () => sub.unsubscribe();
+  }, [controller]);
+
+  async function listPost() {
+    const { data, errors } = await client.queries.listDivision({
+      Controller: controller,
+    });
+    console.log('listDivision=', data);
+    if (data) {
+      setPosts(data as Array<{ Division: string; DivisionName: string; Geojson: string; Controller?: string | null }>); // 型を明示的にキャストする
+    }
+  }
+
+  async function addPost() {
+    const { data } = await client.mutations.addDivision(
+      {
+        DivisionName: window.prompt("DivisionName"),
+        Controller: window.prompt("Controller"),
+      },
+      { authMode: "apiKey" }
+    );
+    console.log("add=", data);
+  }
+
+  return (
+    <main>
+      <h1>Device</h1>
+      <button onClick={addPost}>+ new post</button>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.Controller}>
+            {post.Division}{post.DivisionName}{post.Geojson}{post.Controller}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+
