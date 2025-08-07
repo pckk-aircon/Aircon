a378307cd1debc1b18f2be2d1792ba8076aefd67
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-08-07 15:35:10 +0900

    camera

--- app/Map3/page.tsx
+++ app/Map3/page.tsx
@@ -215,7 +215,7 @@ export default function App(): JSX.Element {
             render(gl: WebGLRenderingContext, args: any) {
               try {
                 engine.wipeCaches();
-                camera._projectionMatrix = BABYLON.Matrix.FromArray(args.defaultProjectionData.mainMatrix);
+                camera.freezeProjectionMatrix(BABYLON.Matrix.FromArray(args.defaultProjectionData.mainMatrix));
                 scene.render(false);
                 map.triggerRepaint();
               } catch (error) {
@@ -234,7 +234,6 @@ export default function App(): JSX.Element {
 
   return <div id="map" style={{ height: '80vh', width: '80%' }} />;
 }
-
 
 function createCombinedQuaternionFromDirection(directionRaw: string): BABYLON.Quaternion {
   let direction: [number, number, number] = [0, 0, 0];
