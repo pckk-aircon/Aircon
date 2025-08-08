4a2678fcdb0bc61b591c766af62406087b6c0f5f
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2025-08-08 19:05:50 +0900

    three-glb-maluti-pul

--- app/Map4/page.tsx
+++ app/Map4/page.tsx
@@ -19,154 +19,6 @@ const ThreeDModelMap: React.FC = () => {
 
     const map = new maplibregl.Map({
       container: mapContainerRef.current,
-      style:
-        //'https://api.maptiler.com/maps/basic/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
-        'https://api.maptiler.com/maps/basic/style.json?key=rtAeicf6fB2vbuvHChpL',
-        
-      zoom: 18,
-      center: [140.302994, 35.353503],
-      pitch: 60,
-      canvasContextAttributes: { antialias: true }
-    });
-
-    const modelOrigin: [number, number] = [140.302994, 35.353503];
-    const modelAltitude = 0;
-    const modelRotate: [number, number, number] = [Math.PI / 2, 0, 0];
-
-    const modelAsMercatorCoordinate = MercatorCoordinate.fromLngLat(
-      modelOrigin,
-      modelAltitude
-    );
-
-    const modelTransform = {
-      translateX: modelAsMercatorCoordinate.x,
-      translateY: modelAsMercatorCoordinate.y,
-      translateZ: modelAsMercatorCoordinate.z,
-      rotateX: modelRotate[0],
-      rotateY: modelRotate[1],
-      rotateZ: modelRotate[2],
-      scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
-    };
-
-    const baseLayer: CustomLayerInterface = {
-      id: '3d-model',
-      type: 'custom',
-      renderingMode: '3d',
-      onAdd(map, gl) {},
-      render(gl, args) {}
-    };
-
-    const customLayer = Object.assign(baseLayer, {
-      camera: new THREE.Camera(),
-      scene: new THREE.Scene(),
-      renderer: undefined as unknown as THREE.WebGLRenderer,
-      map: map,
-      onAdd(map: Map, gl: WebGLRenderingContext) {
-        this.camera = new THREE.Camera();
-        this.scene = new THREE.Scene();
-
-        const directionalLight = new THREE.DirectionalLight(0xffffff);
-        directionalLight.position.set(0, -70, 100).normalize();
-        this.scene.add(directionalLight);
-
-        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-        directionalLight2.position.set(0, 70, 100).normalize();
-        this.scene.add(directionalLight2);
-
-        //const modelUrl = "AirconModel.gltf";
-        const modelUrl = "AirconModel.glb";
-        const fullModelUrl = "https://pckk-device.s3.ap-southeast-2.amazonaws.com/" + modelUrl;
-
-        const loader = new GLTFLoader();
-        loader.load(
-          //'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
-          fullModelUrl,
-          (gltf) => {
-            this.scene.add(gltf.scene);
-          }
-        );
-
-
-        this.renderer = new THREE.WebGLRenderer({
-          canvas: map.getCanvas(),
-          context: gl,
-          antialias: true
-        });
-        this.renderer.autoClear = false;
-      },
-      render(gl: WebGLRenderingContext, args: any) {
-        const rotationX = new THREE.Matrix4().makeRotationAxis(
-          new THREE.Vector3(1, 0, 0),
-          modelTransform.rotateX
-        );
-        const rotationY = new THREE.Matrix4().makeRotationAxis(
-          new THREE.Vector3(0, 1, 0),
-          modelTransform.rotateY
-        );
-        const rotationZ = new THREE.Matrix4().makeRotationAxis(
-          new THREE.Vector3(0, 0, 1),
-          modelTransform.rotateZ
-        );
-
-        const m = new THREE.Matrix4().fromArray(args.defaultProjectionData.mainMatrix);
-        const l = new THREE.Matrix4()
-          .makeTranslation(
-            modelTransform.translateX,
-            modelTransform.translateY,
-            modelTransform.translateZ
-          )
-          .scale(
-            new THREE.Vector3(
-              modelTransform.scale,
-              -modelTransform.scale,
-              modelTransform.scale
-            )
-          )
-          .multiply(rotationX)
-          .multiply(rotationY)
-          .multiply(rotationZ);
-
-        this.camera.projectionMatrix = m.multiply(l);
-        this.renderer.resetState();
-        this.renderer.render(this.scene, this.camera);
-        this.map.triggerRepaint();
-      }
-    });
-
-    map.on('style.load', () => {
-      map.addLayer(customLayer);
-    });
-
-    return () => map.remove();
-  }, []);
-
-  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
-};
-
-export default ThreeDModelMap;
-
-*/
-
-
-"use client";
-
-import React, { useEffect, useRef } from 'react';
-import maplibregl, {
-  Map,
-  MercatorCoordinate,
-  CustomLayerInterface
-} from 'maplibre-gl';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-const ThreeDModelMap: React.FC = () => {
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (!mapContainerRef.current) return;
-
-    const map = new maplibregl.Map({
-      container: mapContainerRef.current,
       style: 'https://api.maptiler.com/maps/basic/style.json?key=rtAeicf6fB2vbuvHChpL',
       zoom: 18,
       center: [140.302994, 35.353503],
@@ -262,5 +114,138 @@ const ThreeDModelMap: React.FC = () => {
 };
 
 export default ThreeDModelMap;
+
+*/
+
+
+"use client";
+
+import React, { useEffect, useRef } from 'react';
+import maplibregl, {
+  Map,
+  MercatorCoordinate,
+  CustomLayerInterface
+} from 'maplibre-gl';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const ThreeDModelMap: React.FC = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: 'https://api.maptiler.com/maps/basic/style.json?key=rtAeicf6fB2vbuvHChpL',
+      zoom: 18,
+      center: [140.302994, 35.353503],
+      pitch: 60,
+      canvasContextAttributes: { antialias: true }
+    });
+
+    const modelRotate: [number, number, number] = [Math.PI / 2, 0, 0];
+
+    const baseLayer: CustomLayerInterface = {
+      id: '3d-model',
+      type: 'custom',
+      renderingMode: '3d',
+      onAdd(map, gl) {},
+      render(gl, args) {}
+    };
+
+    const customLayer = Object.assign(baseLayer, {
+      camera: new THREE.Camera(),
+      scene: new THREE.Scene(),
+      renderer: undefined as unknown as THREE.WebGLRenderer,
+      map: map,
+      onAdd(map: Map, gl: WebGLRenderingContext) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        const modelUrl = "AirconModel.glb";
+        const fullModelUrl = "https://pckk-device.s3.ap-southeast-2.amazonaws.com/" + modelUrl;
+
+        const loader = new GLTFLoader();
+        loader.load(
+          fullModelUrl,
+          (gltf) => {
+            const originalModel = gltf.scene;
+
+            const locations: [number, number][] = [
+              [140.302994, 35.353503],
+              [140.303500, 35.353800],
+              [140.304000, 35.354100]
+            ];
+
+            locations.forEach((lngLat) => {
+              const mercator = MercatorCoordinate.fromLngLat(lngLat, 0);
+              const scale = mercator.meterInMercatorCoordinateUnits();
+
+              const model = originalModel.clone();
+              model.matrixAutoUpdate = false;
+
+              const translation = new THREE.Matrix4().makeTranslation(
+                mercator.x,
+                mercator.y,
+                mercator.z
+              );
+              const rotationX = new THREE.Matrix4().makeRotationX(modelRotate[0]);
+              const rotationY = new THREE.Matrix4().makeRotationY(modelRotate[1]);
+              const rotationZ = new THREE.Matrix4().makeRotationZ(modelRotate[2]);
+              const scaleMatrix = new THREE.Matrix4().makeScale(scale, -scale, scale);
+
+              model.matrix = new THREE.Matrix4()
+                .multiply(translation)
+                .multiply(rotationX)
+                .multiply(rotationY)
+                .multiply(rotationZ)
+                .multiply(scaleMatrix);
+
+              this.scene.add(model);
+            });
+          },
+          undefined,
+          (error) => {
+            console.error('モデルの読み込みに失敗しました:', error);
+          }
+        );
+
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true
+        });
+        this.renderer.autoClear = true;
+      },
+      render(gl: WebGLRenderingContext, args: any) {
+        const m = new THREE.Matrix4().fromArray(args.defaultProjectionData.mainMatrix);
+        this.camera.projectionMatrix = m;
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      }
+    });
+
+    map.on('style.load', () => {
+      map.addLayer(customLayer);
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default ThreeDModelMap;
+
 
 
