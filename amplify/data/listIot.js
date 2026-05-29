e8063a0a9540b83c9b53cea9fb941c175dd25ed1
田渕 潔 <kiyoshi.tabuchi@tk.pacific.co.jp>
2026-05-29 11:18:58 +0900

    start

--- amplify/data/listIot.js
+++ amplify/data/listIot.js
@@ -1,43 +1,5 @@
 /*
 
-import { util } from '@aws-appsync/utils';
-
-export function request(ctx) {
-  return {
-    operation: 'Query',
-    query: {
-      expression: 'Controller = :controller AND DeviceDatetime BETWEEN :startDatetime AND :endDatetime',
-      expressionValues: util.dynamodb.toMapValues({ 
-        ':controller': ctx.args.Controller,
-        ':startDatetime': ctx.args.StartDatetime,
-        ':endDatetime': ctx.args.EndDatetime
-      })
-    },
-    index: 'Controller-DeviceDatetime-index'
-  };
-}
-
-export const response = (ctx) => ctx.result.items;
-
-*/
-
-/*
-//NG
-export function request(ctx) {
-  return {
-    operation: 'Query',
-    query: {
-      expression: 'Controller = :controller AND DeviceDatetime >= :startDatetime AND DeviceDatetime <= :endDatetime',
-      expressionValues: util.dynamodb.toMapValues({ 
-        ':controller': ctx.args.Controller,
-        ':startDatetime': ctx.args.StartDatetime,
-        ':endDatetime': ctx.args.EndDatetime
-      })
-    },
-    index: 'Controller-DeviceDatetime-index'
-  };
-}
-*/
 
 import { util } from '@aws-appsync/utils';
 
@@ -60,4 +22,24 @@ export function request(ctx) {
 
 export const response = (ctx) => ctx.result.items;
 
+*/
 
+import { util } from '@aws-appsync/utils';
+
+export function request(ctx) {
+  return {
+    operation: 'Query',
+    query: {
+      expression: 'Controller = :controller AND DeviceDatetime >= :startDatetime',
+      expressionValues: util.dynamodb.toMapValues({ 
+        ':controller': ctx.args.Controller,
+        ':startDatetime': ctx.args.StartDatetime
+      })
+    },
+    index: 'Controller-DeviceDatetime-index',
+    limit: 10000,
+    scanIndexForward: false
+  };
+}
+
+export const response = (ctx) => ctx.result.items;
